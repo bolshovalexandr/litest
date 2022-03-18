71afb0a3ca6858ed53b666e3882757a91f0f0711
Bolshov Alexander <bolshov@inbox.ru>
2022-03-18 23:28:37 +0300

    candles

--- components/to-the-moon/animation.ts
+++ components/to-the-moon/animation.ts
@@ -16,146 +16,140 @@
 // ======== 1 ИСХОДНЫЕ   =======
 const CANVAS_WIDTH = 900;
 const CANVAS_HEIGHT = 200;
-const CANDLE_LINE_MAX_HEIGHT = CANVAS_HEIGHT * 0.8;
-const CANDLE_WIDTH = 10;
 
 // const TIME = 1200;
 
 // Изменение координат ракеты
 const rocket = {
-	MIX_Y: 45,
+	MIN_Y: 45,
 	MAX_Y: 105,
+	x: 0,
 	y: 51,
+	xMotion: 'go',
 	yDirection: 'down',
 	update() {
 		if (this.yDirection === 'down' && this.y === this.MAX_Y) {
 			this.yDirection = 'up';
 		}
-		if (this.yDirection === 'up' && this.y === this.MIX_Y) {
+		if (this.yDirection === 'up' && this.y === this.MIN_Y) {
 			this.yDirection = 'down';
 		}
-		this.y = this.y + (this.yDirection === 'down' ? 1.5 : -2);
-		return this.y;
-	}
-};
 
-/**
- * СВЕЧА - ИТЕРАЦИЯ
- *
- *
- */
-const candleIteration = {
-	x: 220,
-	iterationRandom: Math.random(),
-	// y: 0,
-	update() {
-		// получаем случайную высоту линии в пределах CANDLE_LINE_MAX_HEIGHT, считаем от нее тело свечи и случайно определяем цвет
-		if (this.x === CANVAS_WIDTH) {
-			this.x = 0;
-			this.iterationRandom = Math.random();
+		if (this.x > CANVAS_WIDTH / 4) {
+			this.y = this.y + (this.yDirection === 'down' ? 2 : -2);
 		}
-		this.x = this.x += 1;
+
+		if (this.xMotion === 'go' && this.x < CANVAS_WIDTH / 2) {
+			this.x += 2;
+		} else {
+			this.xMotion = 'stay';
+		}
+
+		if (this.xMotion === 'stay') {
+			if (this.yDirection === 'up') {
+				if (this.y < (this.MAX_Y - this.MIN_Y)) {
+					this.x += 0.5;
+				} else {
+					this.x -= 0.5;
+				}
+			}
+			if (this.yDirection === 'down') {
+				if (this.y > (this.MAX_Y - this.MIN_Y)) {
+					this.x += 0.5;
+				} else {
+					this.x -= 0.5;
+				}
+			}
+		}
 
 		return {
 			x: this.x,
-			iterationRandom: this.iterationRandom
+			y: this.y
 		};
 	}
 };
-
-/**
- * мне нужен класс, объект которого будет возвращать массив координат для построения свечей
- * массив возвращается для каждого такта (requestAnimationFrame)
- * на каждый такт приходится набор свечей в соответствующих положениях
- *
- * конструктор принимает
- *
- */
 
 /**
  * алгортим
  * - принимаем ширину холста и количество свечей
- * -
+ * - рассчитываем расстояние между свечами так, чтобы они появлялись равномерно
  */
 
 class Candles {
 	private readonly candleWidth = 20;
+	private readonly candleOffsetTop = 20;
 	private readonly candleHeight = 100;
-	candles: {x: number, y: number, w: number, h: number}[] = [];
+	private gap: number;
+	private candles: {x: number, y: number, w: number, h: number, line?: {x: number; y1: number; y2: number}}[] = [];
 
 	constructor(private width: number, private quantity: number) {
-
+		this.gap = (width - (quantity - 1) * this.candleWidth) / (quantity);
+		this.addNext();
 	}
 
-	private get first() {
+	private get next() {
 		return {
 			x: this.width + this.candleWidth,
-			y: 20,
+			y: this.candleOffsetTop,
 			w: this.candleWidth,
-			h: this.candleHeight
+			h: this.candleHeight,
+			color: Math.random() > 0.5 ? 'green' : 'red'
 		};
 	}
 
-	updateCoords() {
-		this.candles.forEach(candle => candle.x--);
+	get items() {
+		return this.candles.map((candle) => {
+			candle.line = {
+				x: candle.x + candle.w / 2,
+				y1: candle.y - 10,
+				y2: candle.y + candle.h + 10
+			};
+			return candle;
+		});
 	}
 
-	dropFirst() {}
-	add() {}
+	addNext() {
+		this.candles.push(this.next);
+	}
+
+	dropFirst() {
+		this.candles.shift();
+	}
+
+	update() {
+		this.candles.forEach(candle => (candle.x -= 2));
+		if (this.candles[this.candles.length - 1].x < this.width - this.gap && this.candles.length < this.quantity) {
+			this.addNext();
+		}
+		if (this.candles[0].x + this.candleWidth < 0) {
+			this.dropFirst();
+		}
+	}
 }
 
-/** рисуем свечу
- *
- *
- */
-const getCandle = (ctx: CanvasRenderingContext2D, xCandle, heightCandle, colorCandle, candleLineOffsetTop) => {
-	ctx.fillStyle = colorCandle;
-	ctx.strokeStyle = colorCandle;
-
-	// опорная линия
-	// отступ сверху: (высота канваса - полная высота свечи) * случайную величину (пока, потом должна быть синусоида)
-	ctx.beginPath();
-	ctx.moveTo(CANVAS_WIDTH - xCandle + CANDLE_WIDTH / 2, candleLineOffsetTop);
-	ctx.lineTo(CANVAS_WIDTH - xCandle + CANDLE_WIDTH / 2, candleLineOffsetTop + heightCandle);
-	ctx.stroke();
-
-	// тело свечи - прямоугольник
-	const cBodyX = CANVAS_WIDTH - xCandle;
-	const cBodyY = candleLineOffsetTop + 10;
-	const cBodyW = CANDLE_WIDTH;
-	const cBodyH = heightCandle - 20;
-	ctx.fillRect(cBodyX, cBodyY, cBodyW, cBodyH);
-};
+const candle = new Candles(900, 12);
 
 // ======== 3 ОТРИСОВКА =======
 const drawScene = (ctx: CanvasRenderingContext2D, img: HTMLImageElement) => {
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	const yRocket = rocket.update();
-
-	// отрисовка порции свечей
-	const candleSetRandom = Math.random();
-	const candleCoords: {xCandle: number, heightCandle: number, colorCandle: 'red' | 'green', candleSetRandom: number, iterationRandom: number}[] = [];
-	for (let i = 0; i < 6; i++) {
-		const { x, iterationRandom } = candleIteration.update();
-		const xCandle = x - i * 25;
-		const colorCandle = candleSetRandom > 0.5 ? 'green' : 'red';
-		const heightCandle = Math.floor(CANDLE_LINE_MAX_HEIGHT * iterationRandom);
-		const candleLineOffsetTop = Math.floor((CANVAS_HEIGHT - heightCandle) * iterationRandom);
-
-		candleCoords.push({ xCandle, heightCandle, colorCandle, candleLineOffsetTop });
-	}
-	console.log(candleCoords);
-
-	candleCoords.forEach(({ xCandle, heightCandle, colorCandle, candleLineOffsetTop }) => {
-		getCandle(ctx, xCandle, heightCandle, colorCandle, candleLineOffsetTop);
-	});
+	const { x: xRocket, y: yRocket } = rocket.update();
 
 	// вставляем изображение
 	// считаем, чтобы длина изображения была 20% от длины холста
 	const w = (CANVAS_WIDTH / 100) * 15;
 	const h = (img.height / img.width) * w;
-	ctx.drawImage(img, (CANVAS_WIDTH / 2) - w / 2, yRocket, w, h);
-
+	candle.update();
+	candle.items.forEach(({ x, y, w, h, line, color }) => {
+		ctx.fillStyle = color;
+		ctx.strokeStyle = color;
+		ctx.beginPath();
+		ctx.moveTo(line?.x, line?.y1);
+		ctx.lineTo(line?.x, line?.y2);
+		ctx.stroke();
+		ctx.fillRect(x, y, w, h);
+	});
+	ctx.drawImage(img, xRocket - w / 2, yRocket, w, h);
+	console.log(candle.items);
 	requestAnimationFrame(() => {
 		drawScene(ctx, img);
 	});
